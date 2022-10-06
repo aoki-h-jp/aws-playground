f47010406d30e3b4fb0b762322c2cde2181488c0
aoki-h-jp <aoki.hirotaka.biz@gmail.com>
2022-10-07 07:34:38 +0900

    slightly fix

--- python-w3resource-exercises/python-basic-part-1/018.py
+++ python-w3resource-exercises/python-basic-part-1/018.py
@@ -8,7 +8,7 @@ def main():
     given_num1 = float(input('Enter number:'))
     given_num2 = float(input('Enter number:'))
     given_num3 = float(input('Enter number:'))
-    return given_num1 * 3 if given_num1 == given_num2 == given_num3 else given_num1 + given_num2 * given_num3
+    return given_num1 * 3 if given_num1 == given_num2 == given_num3 else given_num1 + given_num2 + given_num3
 
 
 if __name__ == '__main__':
